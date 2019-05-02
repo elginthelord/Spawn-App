cf0b657b732b997c93e561ef292d7f79ff13226e
DANIEL HAMILL <daniel.hamill22@clcmbp8.local>
2019-05-02 12:55:11 -0500

    added the projectile function

--- Spawn App/GameScene.swift
+++ Spawn App/GameScene.swift
@@ -9,15 +9,24 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
-    
-    
+    struct PhysicsCategory{
+        static let none: UInt32 = 0
+        static let all: UInt32 = UInt32.max
+        static let enemies: UInt32 = 2 // #1
+        static let fireBall: UInt32 = 4 // #2
+        static let player: UInt32 = 1
+}
+        class GameScene: SKScene, SKPhysicsContactDelegate {
    
-    
+    var player = SKSpriteNode(imageNamed: "hero")
+            
     override func didMove(to view: SKView) {
         backgroundColor = UIColor.white
         button()
-        
+        physicsWorld.contactDelegate = self
+        physicsWorld.gravity = CGVector.zero //set gravity of entire physics world to 0
+        run(SKAction.repeatForever(
+            SKAction.sequence([SKAction.run(spawnEnemies), SKAction.wait(forDuration: 1.0)])))
         
        
         }
@@ -69,8 +78,29 @@ class GameScene: SKScene {
         
     
 }
-
-    
-    
-    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else{ return}
+        }
+    let touchLocation = touch.location(in: self)
+    let fireBall = SKSpriteNode(imageNamed: "fireball")
+    fireBall.position = player.position
+    fireBall.physicsBody = SKPhysicsBody(rectangleOf: fireBall.size)
+    fireBall.physicsBody?.mass = 1
+    fireBall.physicsBody?.categoryBitMask = PhysicsCategory.fireBall
+    fireBall.physicsBody?.contactTestBitMask = PhysicsCategory.monster
+    fireBall.physicsBody?.collisionBitMask = PhysicsCategory.none
+    fireBall.physicsBody?.isDynamic = true
+    addChild(fireBall)
+    let offset = touchLocation - fireBall.position
+    let direction = offset.normalized()
+    let shootAmount = direction * 1000
+    let realDest = shootAmount + fireBall.position
+    let actionMoved = SKAction.move(to: realDest, duration: TimeInterval(2.0))
+    let actionMovedDone = SKAction.removeFromParent()
+    fireBall.run(SKAction.sequence([actionMoved, actionMovedDone]))
 }
+    
+    
+    
+
+
