68da2c7660d6182704159ba2be27ba9aa396bdff
DANIEL HAMILL <daniel.hamill22@clcmbp8.local>
2019-05-06 13:17:11 -0500

    added lines of code

--- Spawn App/GameScene.swift
+++ Spawn App/GameScene.swift
@@ -51,7 +51,7 @@ import GameplayKit
         
     }
     func createHealthBar(){ //creates health bar for enemies and home base
-                
+    //SKAction.repeatForever(SKAction.sequence([SKAction.moveByX(moveAmount, y: 0, duration: 0.5), SKAction.waitForDuration(0.5)]))
             }
     
     
